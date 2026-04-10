bd91d1a07454ce8e0ac8f15b77130a137f4a2432
opatricpires <patric.pires@outlook.com>
2026-04-10 00:00:28 -0300

    feat: add initial favorite styles to book card

--- src/app/components/book-card/book-card.ts
+++ src/app/components/book-card/book-card.ts
@@ -4,11 +4,18 @@ import { Component } from '@angular/core';
   selector: 'app-book-card',
   imports: [],
   templateUrl: './book-card.html',
-  styleUrl: './book-card.css',
+  styleUrls: ['./book-card.css'],
 })
 export class BookCardComponent {
+
+  changeFavoriteStatus() {
+    this.book.isFavorite = !this.book.isFavorite;
+  }
+
   book = {
     title: 'The Great Gatsby',
     author: 'F. Scott Fitzgerald',
+    isFavorite: false,
+    image: "https://m.media-amazon.com/images/I/71OiLUfqjqL._AC_UF1000,1000_QL80_.jpg",
   }
 }
